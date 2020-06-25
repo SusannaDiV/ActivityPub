47d7a107835431910ede319f65e6c4aa58f36a4b
Susanna Di Vita <susanna.divita.2@gmail.com>
2020-06-25 18:16:51 +0200

    Add files via upload

---
+++ Activitypub_polls.php
@@ -0,0 +1,58 @@
+<?php
+// This file is part of GNU social - https://www.gnu.org/software/social
+//
+// GNU social is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// GNU social is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with GNU social.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * ActivityPub implementation for GNU social
+ *
+ * @package   GNUsocial
+ * @author    Diogo Cordeiro <diogo@fc.up.pt>
+ * @copyright 2018-2019 Free Software Foundation, Inc http://www.fsf.org
+ * @license   https://www.gnu.org/licenses/agpl.html GNU AGPL v3 or later
+ * @link      http://www.gnu.org/software/social/
+ */
+
+defined('GNUSOCIAL') || die();
+
+/**
+ * ActivityPub Attachment representation
+ *
+ * @category  Plugin
+ * @package   GNUsocial
+ * @author    Diogo Cordeiro <diogo@fc.up.pt>
+ * @license   https://www.gnu.org/licenses/agpl.html GNU AGPL v3 or later
+ */
+class Activitypub_poll
+{
+    /**
+     * Generates a pretty array from an Attachment object
+     *
+     * @author Diogo Cordeiro <diogo@fc.up.pt>
+     * @param Attachment $attachment
+     * @return array pretty array to be used in a response
+     */
+    public static function poll_to_array($poll)
+    {
+        $res = [
+            '@context' => 'https://www.w3.org/ns/activitystreams',
+            'type'      => 'Question',
+            'id' => $poll->mimetype,
+            'name'      => $poll->getTitle(),
+            'content' => $poll->question,
+            'oneOf' => $poll->options
+        ];
+        return $res;
+    }
+}
